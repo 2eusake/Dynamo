83d26380baaa14370925ad8d0ca4c5fa6b04f333
lutho <mbtlut003@myuct.ac.za>
2024-09-05 17:21:04 +0200

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import Navbar from './components/Common/Navbar';
 import Sidebar from './components/Dashboard/Sidebar';
 import ProjectProvider from './contexts/ProjectContext';
 import TaskProvider from './contexts/TaskContext';
-import SettingsPage from './components/Settings/SettingsPage';
+
 
 import './tailwind.css';
 
@@ -40,7 +40,6 @@ const App = () => {
                 <Route path="/projects/edit/:id" element={<ProtectedRoute element={<EditProject />} />} />
                 <Route path="/create-project" element={<ProtectedRoute element={<CreateProject />} />} />
                 <Route path="/tasks/:id" element={<ProtectedRoute element={<TaskDetail />} />} />
-                <Route path="/settings" element={<ProtectedRoute element={<SettingsPage />} />} />
               </Routes>
             </TaskProvider>
           </ProjectProvider>
