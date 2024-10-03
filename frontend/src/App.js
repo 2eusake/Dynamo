30da1d6e919b28b5ef76d08874c627684146b51b
iviwe <blnivi001@myuct.ac.za>
2024-10-03 14:48:51 +0200

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,231 @@
+// import React, { useContext, useState } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   useNavigate,
+// } from "react-router-dom"; // Correct import
+// import { AuthProvider, AuthContext } from "./contexts/AuthContext"; // AuthProvider
+// import Login from "./components/Auth/Login";
+// import Register from "./components/Auth/Register";
+// import Dashboard from "./components/Dashboard/Dashboard";
+// import ProjectDetail from "./components/Project/ProjectDetail";
+// import ProjectList from "./components/Project/ProjectList";
+// import CreateProject from "./components/Project/CreateProject";
+// import EditProject from "./components/Project/EditProject";
+// import TaskDetail from "./components/Task/TaskDetail";
+// import TasksPage from "./components/Task/TasksPage";
+// import ProtectedRoute from "./protectedRoute";
+// import Footer from "./components/Common/Footer";
+// import Navbar from "./components/Common/Navbar";
+// import Sidebar from "./components/Dashboard/Sidebar";
+// import ProjectProvider from "./contexts/ProjectContext";
+// import ReportsPage from "./components/Reports/ReportsPage";
+// import TaskProvider from "./contexts/TaskContext";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import "./tailwind.css";
+// import Settings from "./components/Settings/SettingsPage";
+// import EditProfilePage from "./components/Settings/EditProfilePage";
+// import ResetPasswordPage from "./components/Settings/ResetPasswordPage";
+// import { UserProvider } from "./contexts/UserContext"; // Ensure correct path
+// import Spinner from "./components/Common/Spinner";
+// import "./App.css";
+
+
+// const App = () => {
+//   return (
+//     <AuthProvider>
+//       <Router>
+//         <AuthContext.Consumer>
+//           {({ user, loading }) => {
+//             if (loading) {
+//               return <Spinner />;
+//             }
+
+//             return (
+//               <>
+//                 {user && <Navbar />}
+//                 <div classname="bg-deloitte-white text-deloitte-black min-h-screen flex">
+//                   {user && <Sidebar />}
+//                   <div className={`flex-1 ${user ? "ml-48" : ""} p-4`}>
+//                     <ProjectProvider>
+//                       <TaskProvider>
+//                         <UserProvider>
+//                           <Routes>
+//                             <Route path="/" element={<Login />} />
+//                             <Route path="/login" element={<Login />} />
+//                             <Route path="/register" element={<Register />} />
+//                             <Route
+//                               path="/dashboard"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<Dashboard />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/projects"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<ProjectList />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/projects/:id"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<ProjectDetail />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/projects/edit/:id"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<EditProject />}
+//                                   allowedRoles={["Director", "Project Manager"]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/create-project"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<CreateProject />}
+//                                   allowedRoles={["Director", "Project Manager"]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               exact
+//                               path="/settings"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<Settings />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/settings/edit-profile"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<EditProfilePage />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/settings/reset-password"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<ResetPasswordPage />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/tasks"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<TasksPage />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/tasks/:taskId"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<TaskDetail />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                             <Route
+//                               path="/reports"
+//                               element={
+//                                 <ProtectedRoute
+//                                   element={<ReportsPage />}
+//                                   allowedRoles={[
+//                                     "Director",
+//                                     "Project Manager",
+//                                     "Consultant",
+//                                   ]}
+//                                 />
+//                               }
+//                             />
+//                           </Routes>
+//                         </UserProvider>
+//                       </TaskProvider>
+//                     </ProjectProvider>
+//                   </div>
+//                 </div>
+//               </>
+//             );
+//           }}
+//         </AuthContext.Consumer>
+
+//         <ToastContainer
+//           position="top-right"
+//           autoClose={5000}
+//           hideProgressBar={false}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//         />
+//       </Router>
+//       <Footer />
+//     </AuthProvider>
+//   );
+// };
+
+// export default App;
 import React, { useContext, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useNavigate,
 } from "react-router-dom"; // Correct import
 import { AuthProvider, AuthContext } from "./contexts/AuthContext"; // AuthProvider
 import Login from "./components/Auth/Login";
@@ -16,9 +238,6 @@ import EditProject from "./components/Project/EditProject";
 import TaskDetail from "./components/Task/TaskDetail";
 import TasksPage from "./components/Task/TasksPage";
 import ProtectedRoute from "./protectedRoute";
-import Footer from "./components/Common/Footer";
-import Navbar from "./components/Common/Navbar";
-import Sidebar from "./components/Dashboard/Sidebar";
 import ProjectProvider from "./contexts/ProjectContext";
 import ReportsPage from "./components/Reports/ReportsPage";
 import TaskProvider from "./contexts/TaskContext";
@@ -31,6 +250,7 @@ import ResetPasswordPage from "./components/Settings/ResetPasswordPage";
 import { UserProvider } from "./contexts/UserContext"; // Ensure correct path
 import Spinner from "./components/Common/Spinner";
 import "./App.css";
+import Layout from './components/Layout'; // Import the Layout component
 
 const App = () => {
   return (
@@ -44,19 +264,20 @@ const App = () => {
 
             return (
               <>
-                {user && <Navbar />}
-                <div className="bg-deloitte-white text-deloitte-black min-h-screen flex">
-                  {user && <Sidebar />}
-                  <div className={`flex-1 ${user ? "ml-48" : ""} p-4`}>
-                    <ProjectProvider>
-                      <TaskProvider>
-                        <UserProvider>
-                          <Routes>
-                            <Route path="/" element={<Login />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route
-                              path="/dashboard"
+                <ProjectProvider>
+                  <TaskProvider>
+                    <UserProvider>
+                      <Routes>
+                        {/* Routes that don't need Layout */}
+                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+
+                        {/* Routes that need Layout */}
+                        <Route
+                          path="/dashboard"
+                          element={
+                            <ProtectedRoute
                               element={
                                 <Layout>
                                   <Dashboard />
@@ -217,7 +438,6 @@ const App = () => {
           pauseOnHover
         />
       </Router>
-      <Footer />
     </AuthProvider>
   );
 };
