21469b133e9c8fab3ff3bed6dfd506fc26766c92
Zusake Bangani <145461228+2eusake@users.noreply.github.com>
2024-10-28 10:08:18 +0200

    database mods

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,22 +3,15 @@ const fs = require("fs");
 
 //KPiu7qTJnGs51CGDX7ab
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,       
-  process.env.DB_USER,       
-  process.env.DB_PASS,       
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql",
-    port: process.env.DB_PORT || 3306, 
-    dialectOptions: {
-      ssl: {
-        
-        ca: fs.readFileSync(path.join(__dirname, "eu-north-1-bundle.pem")),
-      },
-    },
-    logging: false, 
-  }
+const sequelize = new Sequelize('dpms_serv', 'zeusake', 'KPiu7qTJnGs51CGDX7ab', {
+  host: 'dynamo.c324ec2065m6.eu-north-1.rds.amazonaws.com',
+  dialect: 'mysql',
+  port: 5000,
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync('config/eu-north-1-bundle.pem') 
+    }
+  }}
 );
 
 sequelize
